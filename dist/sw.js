9bea38dd0e3aaa6ead14df5726a2f64d9f363a63
Shawn Stickle <sestickl@gmail.com>
2025-02-18 13:34:27 -0600

    Branding update

--- dist/sw.js
+++ dist/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './t6_training.js',
+  './t6_training_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
