6c4c16297fa169d336c8f0548e4761b5448c9acb
Shawn Stickle <sestickl@gmail.com>
2025-02-26 00:34:26 -0600

    Update t6_training.js

--- t6_training.js
+++ t6_training.js
@@ -1630,7 +1630,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('t6_training_bg.wasm', import.meta.url);
+        module_or_path = new URL('t6_training_bg.wasm?v=2', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
